89d5c6c843091f51f5c7b62b600ae65dbe1222c0
Jordi Gasparin <gascart@gmail.com>
2011-11-01 15:50:44 +0100

    Add sql_radical

---
+++ sql_radical/events/clear/brewfest_clear.sql
@@ -0,0 +1,7 @@
+DELETE FROM `gameobject` WHERE `id` = 189990;
+DELETE FROM `creatures` WHERE `id` = 15248;
+DELETE FROM `creatures` WHERE `id` = 15255;
+DELETE FROM `creatures` WHERE `id` = 15256;
+DELETE FROM `creatures` WHERE `id` = 15257;
+DELETE FROM `creatures` WHERE `id` = 15258;
+DELETE FROM `creatures` WHERE `id` = 15259;
---
+++ sql_radical/events/clear/halloween_clear.sql
@@ -0,0 +1,1 @@
+DELETE FROM `creatures` WHERE `id` = 24575;
