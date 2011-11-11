c5e861b32982dfd6630933717a36b24b7fbb1000
Raynar RS <davis87@correo.ugr.es>
2011-11-11 09:54:39 +0100

    Fix overbug damage of Mirror image. (Tempfix)

---
+++ sql_radical/0008_mirror_image.sql
@@ -0,0 +1,3 @@
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
+(59637, 0, 0, 0, 0, 'SPELL_FIREBLAST'),
+(59638, 0, 0, 0, 0, 'SPELL_FROSTBOLT');
