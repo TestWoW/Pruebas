580540c3ed9c1a8b1b85ce928e396459c21e0bfc
Raynar RS <davis87@correo.ugr.es>
2011-11-12 18:09:44 +0100

    Bone Shield proc.

---
+++ sql_radical/0009_mangos_boneshield.sql
@@ -0,0 +1,3 @@
+delete form spell_proc_event where entry=49222;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMaskA0`, `SpellFamilyMaskA1`, `SpellFamilyMaskA2`, `SpellFamilyMaskB0`, `SpellFamilyMaskB1`, `SpellFamilyMaskB2`, `SpellFamilyMaskC0`, `SpellFamilyMaskC1`, `SpellFamilyMaskC2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(49222, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 139944, 0, 0, 0, 2);
