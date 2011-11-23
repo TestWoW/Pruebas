db0c56882935f4bc41b00cfc0886dd382e01d9d0
Jordi Gasparin <gascart@gmail.com>
2011-11-23 19:04:05 +0100

    arreglando conflictos y pasando del dev

---
+++ sql_radical/0010_mangos_sagrario_rubi.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `ScriptName` = 'mob_living_ember' WHERE `entry` IN ('40683');
+UPDATE `creature_template` SET `ScriptName` = 'mob_living_inferno' WHERE `entry` IN ('40681');
