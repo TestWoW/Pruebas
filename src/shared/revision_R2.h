09d1e69c14562adf5e5b0ac4ecae3db27a468ada
Jordi Gasparin <gascart@gmail.com>
2011-12-02 18:30:16 +0100

    ups

--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,8 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
-<<<<<<< HEAD
- #define REVISION_R2 "1346"
-=======
  #define REVISION_R2 "1350"
->>>>>>> 99692495adc656c0162a85f25d5dd9b8be6f9c20
 #endif // __REVISION_R2_H__
