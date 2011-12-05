d70281af17cf5ea156d067750b1fa768a0442b2f
Jordi Gasparin <gascart@gmail.com>
2011-12-05 17:26:49 +0100

    arreglados problemas del merge

--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,8 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
-<<<<<<< HEAD
-#define REVISION_R2 "1360"
-=======
  #define REVISION_R2 "1372"
->>>>>>> 94f585f502be304aa49b8cb4df4116056d124e5d
 #endif // __REVISION_R2_H__
