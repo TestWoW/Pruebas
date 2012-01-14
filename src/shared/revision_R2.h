950d1fff24ba1104200a556ba8450c3ee04349e9
Jordi <gascart@gmail.com>
2012-01-14 13:48:58 +0100

    Revert "[mr1540] finished removing implicit casts uint64->ObjectGuid. return to main mangos way."
    This reverts commit 9436b597efba557a9e40cc89cdd4227df7008e23.
    
    Conflicts:
    
    	src/shared/revision_R2.h

--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,4 +1,8 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
+<<<<<<< HEAD
  #define REVISION_R2 "1542"
+=======
+ #define REVISION_R2 "1539"
+>>>>>>> 9436b59... [mr1540] finished removing implicit casts uint64->ObjectGuid. return to main mangos way.
 #endif // __REVISION_R2_H__
