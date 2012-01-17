0b4bc41a6924504dcafbbf536e0a3b271d0ad150
Jordi <gascart@gmail.com>
2012-01-17 20:33:13 +0100

    revision

--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,8 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
-<<<<<<< HEAD
  #define REVISION_R2 "1564"
-=======
- #define REVISION_R2 "1562"
->>>>>>> e758a07... [mr1563] correcting Sacred shield HoT proc
 #endif // __REVISION_R2_H__
