8fa9c5d05f92ff4e15be67a7991e130a3076ac36
Jordi <gascart@gmail.com>
2012-01-25 19:02:24 +0100

    revision

--- src/shared/revision_R2.h
+++ src/shared/revision_R2.h
@@ -1,8 +1,4 @@
 #ifndef __REVISION_R2_H__
 #define __REVISION_R2_H__
-<<<<<<< HEAD
- #define REVISION_R2 "1619"
-=======
  #define REVISION_R2 "1625"
->>>>>>> dd97052b7b8ee0f1f5f5a038347904362eb5dc32
 #endif // __REVISION_R2_H__
