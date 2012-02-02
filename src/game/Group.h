47f5eb844f0b36e455b372fce07ddffa76a53b46
Raynar RS <davis87@correo.ugr.es>
2012-02-02 09:24:22 +0100

    Revert "Revert "[11898] Prevent resetting instances while offline players are in it""
    This reverts commit 1e3d8559ef4e7526549247f4fb9aeb131f4a392c.

--- src/game/Group.h
+++ src/game/Group.h
@@ -225,13 +225,9 @@ class MANGOS_DLL_SPEC Group
             ObjectGuid  guid;
             std::string name;
             uint8       group;
-<<<<<<< HEAD
             GroupFlagMask  flags;
             uint8       roles;
             uint32      lastMap;
-=======
-            bool        assistant;
->>>>>>> parent of d415433... [11898] Prevent resetting instances while offline players are in it
         };
         typedef std::list<MemberSlot> MemberSlotList;
         typedef MemberSlotList::const_iterator member_citerator;
@@ -241,7 +237,6 @@ class MANGOS_DLL_SPEC Group
 
     protected:
         typedef std::set<Player*> InvitesList;
-
         typedef std::vector<Roll*> Rolls;
 
     public:
@@ -352,6 +347,8 @@ class MANGOS_DLL_SPEC Group
         void OfflineReadyCheck();
 
         void RewardGroupAtKill(Unit* pVictim, Player* player_tap);
+
+        bool SetPlayerMap(const ObjectGuid guid, uint32 mapid);
 
         /*********************************************************/
         /***                   LOOT SYSTEM                     ***/
