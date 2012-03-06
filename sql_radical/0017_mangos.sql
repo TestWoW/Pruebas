0d5a90d3db494248449eb8a65dffeb5b8add8f20
Raynar RS <davis87@correo.ugr.es>
2012-03-06 11:14:50 +0100

    spell_linked of will of the forsaken

---
+++ sql_radical/0017_mangos.sql
@@ -0,0 +1,5 @@
+-- Will of the forsaken
+INSERT INTO `spell_linked` (`entry`, `linked_entry`, `type`, `effect_mask`, `comment`) VALUES
+(7744, 72757, 4, 0, 'Will of the forsaken +  Medallion of the horde'),
+(72752, 7744, 4, 0, 'Will of the forsaken +  Medallion of the horde'),
+(72757, 7744, 4, 0, 'Will of the forsaken +  Medallion of the horde');
