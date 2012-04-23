fe464e187991622a5e115749cd2c7661d8f9ccb8
jordi gasparin <gascart@gmail.com>
2012-04-23 18:45:48 +0200

    Revert "[11974] Fix spell targeting for some spells like 20243"
    This reverts commit 806d27be3a3432030561445d4adb42265b66685a.
    
    Conflicts:
    
    	src/shared/revision_nr.h

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,8 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
+<<<<<<< HEAD
  #define REVISION_NR "11975"
+=======
+ #define REVISION_NR "11973"
+>>>>>>> 806d27b... [11974] Fix spell targeting for some spells like 20243
 #endif // __REVISION_NR_H__
