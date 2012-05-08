65f25f194c2bb57052bba215bcc73410144ef9ad
jordi gasparin <gascart@gmail.com>
2012-05-08 21:15:28 +0200

    define SPELL_ATTR_EX6_IGNORE_DETECTION v0.2 https://github.com/mangosR2/mangos/commit/4d8488c69e393181617ca63f9ffaa98f776dac3a

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -5661,7 +5661,7 @@ SpellCastResult Spell::CheckCast(bool strict)
             if (target->IsTaxiFlying())
                 return SPELL_FAILED_BAD_TARGETS;
 
-            if(!m_IsTriggeredSpell && !(m_spellInfo->AttributesEx2 & SPELL_ATTR_EX2_IGNORE_LOS) && VMAP::VMapFactory::checkSpellForLoS(m_spellInfo->Id) && !m_caster->IsWithinLOSInMap(target, false))
+            if(!m_IsTriggeredSpell && !target->IsVisibleTargetForSpell(m_caster, m_spellInfo, strict))
                 return SPELL_FAILED_LINE_OF_SIGHT;
 
             // auto selection spell rank implemented in WorldSession::HandleCastSpellOpcode
@@ -8121,7 +8121,7 @@ bool Spell::CheckTarget( Unit* target, SpellEffectIndex eff )
             // fall through
         case SPELL_EFFECT_RESURRECT_NEW:
             // player far away, maybe his corpse near?
-            if (target != m_caster && !(m_spellInfo->AttributesEx2 & SPELL_ATTR_EX2_IGNORE_LOS) && !target->IsWithinLOSInMap(m_caster))
+            if (target != m_caster && !target->IsVisibleTargetForSpell(m_caster, m_spellInfo))
             {
                 if (!m_targets.getCorpseTargetGuid())
                     return false;
@@ -8143,7 +8143,7 @@ bool Spell::CheckTarget( Unit* target, SpellEffectIndex eff )
             // Get GO cast coordinates if original caster -> GO
             if (target != m_caster)
                 if (WorldObject *caster = GetCastingObject())
-                    if (!(m_spellInfo->AttributesEx2 & SPELL_ATTR_EX2_IGNORE_LOS) && !target->IsWithinLOSInMap(caster, false))
+                    if (!target->IsVisibleTargetForSpell(m_caster, m_spellInfo, false))
                         return false;
             break;
     }
