05f24d0ec0662ee6d2713d3c8c9b33893df7a0cb
raynar <raynar@hotmail.es>
2012-05-14 11:20:17 +0200

    @msn lag fixes. Testing

--- src/game/GridNotifiersImpl.h
+++ src/game/GridNotifiersImpl.h
@@ -40,10 +40,50 @@ inline void MaNGOS::VisibleNotifier::Visit(GridRefManager<T> &m)
 
 inline void MaNGOS::ObjectUpdater::Visit(CreatureMapType &m)
 {
+    uint32 LastUpdateTime;
+    uint32 DiffTime;
+    uint32 MinUpdateTime = 0;
+    uint32 VisitorCount = 0;
+    uint8 VisitCount = 1;
+    std::vector<uint32> LastUpdateTimeList;
     for(CreatureMapType::iterator iter = m.begin(); iter != m.end(); ++iter)
     {
+        VisitorCount++;
+        LastUpdateTime = iter->getSource()->GetLastUpdateTime();
+        if (LastUpdateTime == 0)
+        {
+            iter->getSource()->SetLastUpdateTime();
+            LastUpdateTime = iter->getSource()->GetLastUpdateTime();
+        }
+        LastUpdateTimeList.push_back(LastUpdateTime);
+    }
+    if (VisitorCount == 0)
+        return;
+    std::sort(LastUpdateTimeList.begin(), LastUpdateTimeList.end());
+    if (VisitorCount > 9)
+        MinUpdateTime = LastUpdateTimeList.at(9);
+    else
+        MinUpdateTime = LastUpdateTimeList.at(VisitorCount-1);
+
+    /*if (VisitorCount > 50)
+        for(CreatureMapType::iterator iter = m.begin(); iter != m.end(); ++iter)
+        {
+            sLog.outError("Entry: %u, GUID: %u, VisitorCount: %u", iter->getSource()->GetEntry(),iter->getSource()->GetGUIDLow(), VisitorCount);
+        }*/
+
+    for(CreatureMapType::iterator iter = m.begin(); iter != m.end(); ++iter)
+    {
+        LastUpdateTime = iter->getSource()->GetLastUpdateTime();
+        DiffTime = WorldTimer::getMSTime() - LastUpdateTime;
+        if (DiffTime < (iter->getSource()->IsInCombat() ? 100 : 500) || LastUpdateTime > MinUpdateTime)
+            continue;
+
         WorldObject::UpdateHelper helper(iter->getSource());
-        helper.Update(i_timeDiff);
+        helper.Update(DiffTime);
+        iter->getSource()->SetLastUpdateTime();
+        VisitCount++;
+        if (VisitCount > 10) // Do not visit more than 10 creature
+            break;
     }
 }
 
--- src/game/TargetedMovementGenerator.cpp
+++ src/game/TargetedMovementGenerator.cpp
@@ -215,7 +215,9 @@ bool TargetedMovementGeneratorMedium<T,D>::Update(T &owner, const uint32 & time_
         G3D::Vector3 dest = owner.movespline->FinalDestination();
 
         bool targetMoved = false;
-        if (owner.GetTypeId() == TYPEID_UNIT && ((Creature*)&owner)->IsLevitating())
+        if (dest == Vector3())
+            targetMoved = true;
+        else if (owner.GetTypeId() == TYPEID_UNIT && ((Creature*)&owner)->IsLevitating())
             targetMoved = !i_target->IsWithinDist3d(dest.x, dest.y, dest.z, allowed_dist);
         else
             targetMoved = !i_target->IsWithinDist2d(dest.x, dest.y, allowed_dist);
@@ -287,7 +289,6 @@ void ChaseMovementGenerator<Creature>::Initialize(Creature &owner)
 {
     owner.SetWalk(false);
     owner.addUnitState(UNIT_STAT_CHASE|UNIT_STAT_CHASE_MOVE);
-    _setTargetLocation(owner);
 
     if (owner.getVictim() && !owner.hasUnitState(UNIT_STAT_MELEE_ATTACKING))
         owner.Attack(owner.getVictim(), !owner.IsNonMeleeSpellCasted(true));
