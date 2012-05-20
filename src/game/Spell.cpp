b5d91d4b3d46f82ddbd048db98e5e49c491d854d
jordi gasparin <gascart@gmail.com>
2012-05-20 21:20:32 +0200

    correct merge mr1911

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -8014,16 +8014,15 @@ bool Spell::CheckTarget( Unit* target, SpellEffectIndex eff )
         if (!CheckTargetCreatureType(target))
             return false;
     }
-
-     // check right target                                                                                       // should activ for spells 72034, 72096
-    if (m_spellInfo->HasAttribute(SPELL_ATTR_EX3_TARGET_ONLY_PLAYER) && target->GetTypeId() != TYPEID_PLAYER /*&& m_spellInfo->EffectImplicitTargetA[eff] != TARGET_SCRIPT*/)
-        return false;
     // Check Aura spell req (need for AoE spells)
     if (m_spellInfo->targetAuraSpell && !target->HasAura(m_spellInfo->targetAuraSpell))
         return false;
     if (m_spellInfo->excludeTargetAuraSpell && target->HasAura(m_spellInfo->excludeTargetAuraSpell))
         return false;
     if (m_spellInfo->TargetAuraStateNot && target->HasAura(m_spellInfo->TargetAuraStateNot))
+        return false;
+     // check right target                                                                                       // should activ for spells 72034, 72096
+    if (m_spellInfo->HasAttribute(SPELL_ATTR_EX3_TARGET_ONLY_PLAYER) && target->GetTypeId() != TYPEID_PLAYER /*&& m_spellInfo->EffectImplicitTargetA[eff] != TARGET_SCRIPT*/)
         return false;
 
     // Check targets for not_selectable unit flag and remove
